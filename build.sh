489fd727339d45ac8c684faa25000a6336959694
Nadeem Bhai <nadeem.a@mizzle.io>
2025-11-20 16:25:36 +0530

    update build file

--- build.sh
+++ build.sh
@@ -7,6 +7,11 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 
+python manage.py makemigrations main
+python manage.py migrate main
+
+python manage.py migrate
+
 # Optional: Create superuser (only if not exists)
 # echo "from django.contrib.auth import get_user_model; \
 # User = get_user_model(); \
