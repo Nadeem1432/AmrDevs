c85eed5fc5e0a256d2977898d264ec69401b0674
Nadeem Bhai <nadeem.a@mizzle.io>
2025-11-27 14:36:19 +0530

    debug12

--- build.sh
+++ build.sh
@@ -42,8 +42,9 @@ pip install -r requirements.txt
 echo "Applying database migrations..."
 # Only run the general migrate command (which covers all apps)
 # The --noinput flag prevents any interactive prompts.
-python manage.py migrate --noinput
 # python manage.py migrate main 0011 --fake
+python manage.py migrate main 0012 --fake
+python manage.py migrate --noinput
 echo "Database migrations applied."
 
 # --- 3. Optional: Create superuser (only if not exists) ---
